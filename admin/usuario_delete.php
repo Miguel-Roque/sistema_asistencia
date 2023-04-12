623a099a7edacff8c3dc7d28cea20ee5de4344c8
Jeam06 <jesusangeldonismaslucan@gmail.com>
2023-04-12 13:24:25 -0500

    enviar datos a tabla papelera

--- admin/usuario_delete.php
+++ admin/usuario_delete.php
@@ -3,7 +3,9 @@
 
 	if(isset($_POST['delete'])){
 		$id = $_POST['id'];
-		$sql = "DELETE FROM admin WHERE id = '$id'";
+		$sql = "INSERT INTO papelera
+				SELECT*FROM admin WHERE id='$id';
+				DELETE FROM admin WHERE id='$id'";
 		if($conn->query($sql)){
 			$_SESSION['success'] = 'Usuario deleted successfully';
 		}
@@ -12,7 +14,7 @@
 		}
 	}
 	else{
-		$_SESSION['error'] = 'Select item to delete first';
+		$_SESSION['error'] = 'Seleccione el usuario a eliminar primero';
 	}
 
 	header('location: usuario.php');
